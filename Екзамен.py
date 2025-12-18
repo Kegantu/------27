441a87e24a01575ef9f3f7194eeb5f43ee751782
Kegantu <90145340+Kegantu@users.noreply.github.com>
2025-12-18 23:09:49 +0200

    final

--- Екзамен.py
+++ Екзамен.py
@@ -1,19 +1,24 @@
 n = int(input("Введіть кількість елементів масиву: "))
 array = list(map(float, input("Введіть елементи масиву через пробіл: ").split()))
 
+def findClosestPair(a):
+    min_diff = abs(a[1] - a[0])
+    idx = 0
+
+    for i in range(1, len(a) - 1):
+        diff = abs(a[i + 1] - a[i])
+        if diff < min_diff:
+            min_diff = diff
+            idx = i
+
+    return a[idx], a[idx + 1], min_diff
+
 if len(array) != n:
     print("Помилка: кількість введених елементів не дорівнює n.")
 elif n < 2:
     print("Потрібно щонайменше 2 елементи.")
 else:
-    minDiff = abs(array[1] - array[0])
-    idx = 0
+    x1, x2, diff = findClosestPair(array)
 
-    for i in range(1, n - 1):
-        diff = abs(array[i + 1] - array[i])
-        if diff < minDiff:
-            minDiff = diff
-            idx = i
-
-    print("Найближча пара:", array[idx], "і", array[idx + 1])
-    print("Мінімальна різниця |xi+1 - xi| =", minDiff)
+    print("Найближча пара:", x1, "і", x2)
+    print("Мінімальна різниця |xi+1 - xi| =", diff)
